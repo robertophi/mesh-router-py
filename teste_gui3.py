6258b97beffff6c99fc4d4e81b0bf61e3a4f2b34
robertophi <robertophi@gmail.com>
2019-04-24 16:30:11 -0300

    draw lines between ndoes

--- teste_gui3.py
+++ teste_gui3.py
@@ -5,8 +5,18 @@ root.resizable(0,0)
 #root.state('normal')
 #root.configure(bg = 'green4')
     
+
+class Node():
+    def __init__(self, canvas, event):
+        self.node = canvas.create_rectangle(event.x-20, event.y-20,
+                                    event.x+20, event.y+20,
+                                    outline='white', fill='blue')
+        self.connected_lines = []
+        self.connected_nodes = []
+
+
 def intersect(node, x,y):
-    [x0,y0,x1,y1] = canvas.coords(node)
+    [x0,y0,x1,y1] = canvas.coords(node.node)
     if x > x0 and x < x1 and y > y0 and y < y1:
         print('intersection')
         return True
@@ -14,9 +24,13 @@ def intersect(node, x,y):
         print('no intersection')
         return False
 
-intersected_node = 0
+
+
+line_list = []
 node_list = []
 def click(event):
+    print("Click:",event.x, event.y)
+
     print("Create node")
     global node_list
     [x,y] = [event.x,event.y]
@@ -26,13 +40,47 @@ def click(event):
             intersect_flag = True
             continue
     if intersect_flag == False:
-        new_w = canvas.create_rectangle(event.x-10, event.y-10,
-                                    event.x+10, event.y+10,
-                                    outline='white', fill='blue')
+        new_w = Node(canvas, event)
         node_list.append(new_w)                                
-    print(event.x, event.y)
+    draw_lines()
 
+def draw_lines():
+    global node_list, line_list
+    for line in line_list.copy():
+        canvas.delete(line)
+        line_list.remove(line)
 
+    for node1 in node_list:
+        for node2 in node_list[0:1]:
+            x0,y0, x1, y1 = canvas.coords(node1.node)
+            xs_A = int((x1+x0)/2)
+            ys_A = int((y1+y0)/2)
+            x3,y3, x4, y4 = canvas.coords(node2.node)
+            xs_B = int((x3+x4)/2)
+            ys_B = int((y3+y4)/2)
+            line = canvas.create_line(xs_A,ys_A,xs_B,ys_B)
+            line_list.append(line)
+            node1.connected_lines.append(line)
+            node2.connected_lines.append(line)
+            
+def draw_connected_lines(node):
+    global node_list, line_list
+    for line in node.connected_lines:
+        canvas.delete(line)
+        #line_list.remove(line)
+
+    node1 = node
+    for node2 in node_list[0:1]:
+        x0,y0, x1, y1 = canvas.coords(node1.node)
+        xs_A = int((x1+x0)/2)
+        ys_A = int((y1+y0)/2)
+        x3,y3, x4, y4 = canvas.coords(node2.node)
+        xs_B = int((x3+x4)/2)
+        ys_B = int((y3+y4)/2)
+        line = canvas.create_line(xs_A,ys_A,xs_B,ys_B)
+        line_list.append(line)
+        node1.connected_lines.append(line)
+    canvas.update()
 def drag(event):
     global node_list
     [x,y] = [event.x,event.y]
@@ -40,10 +88,15 @@ def drag(event):
 
     for node in node_list.copy():
         if intersect(node, x,y) == True:
-            [x0,y0,x1,y1] = canvas.coords(node)
+            [x0,y0,x1,y1] = canvas.coords(node.node)
             xs = int((x0+x1)/2)
             ys = int((y0+y1)/2)
-            canvas.move(node, -(xs-x),-(ys-y))
+            canvas.move(node.node, -(xs-x),-(ys-y))
+            draw_connected_lines(node)
+
+    
+    print([c.node for c in node_list])
+
 
     
 
@@ -53,14 +106,16 @@ def delete(event):
     [x,y] = [event.x,event.y]
     for node in node_list.copy():
         if intersect(node, x,y) == True:
-            canvas.delete(node)
+            canvas.delete(node.node)
             node_list.remove(node)
+    draw_lines()
 
 def delete_all(event):
     global node_list
     for node in node_list.copy():
-        canvas.delete(node)
+        canvas.delete(node.node)
         node_list.remove(node)
+    draw_lines()
 
 
 canvas = Canvas(root, width=500, height=500, bg='green')
