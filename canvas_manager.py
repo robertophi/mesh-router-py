936bde0aa3055025e857d4043597f0523505be82
robertophi <robertophi@gmail.com>
2019-04-25 12:39:26 -0300

    working simple meshing

--- canvas_manager.py
+++ canvas_manager.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from solve_dijkstra import Graph
+import numpy as np
 
 nodes_settings = {'router'     : ['red',30],
                   'basic_node' : ['blue',20]
@@ -18,6 +20,9 @@ class Node():
         self.ID = canvas.create_rectangle(event.x-size, event.y-size,
                                     event.x+size, event.y+size,
                                     outline='white', fill=color)
+
+        self.txt = canvas.create_text(event.x, event.y,
+                                    text=str(self.ID))
         self.connected_lines = []
         self.connected_nodes = []
 
@@ -28,8 +33,10 @@ class CanvasManager():
     def __init__(self, canvas, root):
         self.canvas = canvas
         self.root = root
+        self.dijkstra_graph = Graph(0)
 
         self.canvas.bind("<Button-1>", self.create_node)
+        self.canvas.bind("<Button-2>", self.get_connections_list)
         self.canvas.bind("<Button-3>", self.delete_node_canvas)
         self.canvas.bind("<B1-Motion>", self.drag_node_canvas)
         self.canvas.bind("<Double-Button-3>", self.delete_all_canvas)
@@ -49,7 +56,6 @@ class CanvasManager():
             return False
 
     def check_click_intersection(self, x,y):
-
         if self.node_list != []:
             for node in self.node_list:
                 if self.intersect(node, x, y) == True:
@@ -57,22 +63,28 @@ class CanvasManager():
         return -1
 
     def create_router(self, event):
+        '''
+        Callback for Ctrl + left mouse click
+        '''
         print("Click:",event.x, event.y)
         print("Create new router:")
         [x,y] = [event.x,event.y]      
         if self.check_click_intersection(x,y) == -1:
             new_router = Node(self.canvas, event, node_type='router')
             self.node_list.append(new_router)
-        self.draw_all_lines()
+        self.draw_connections()
 
     def create_node(self, event):
+        '''
+        Callback for left mouse click
+        '''
         print("Click:",event.x, event.y)
         print("Create new node:")
         [x,y] = [event.x,event.y]
         if self.check_click_intersection(x,y) == -1:
             new_node = Node(self.canvas, event, node_type='basic_node')
             self.node_list.append(new_node)
-        self.draw_all_lines()
+        self.draw_connections()
 
     def get_node_center(self, node):
         x0,y0, x1, y1 = self.canvas.coords(node.ID)
@@ -80,73 +92,106 @@ class CanvasManager():
         yc = int((y1+y0)/2)
         return xc, yc
               
-    def get_mesh_connection(self):
-        vertices = []
-        for node1 in self.node_list:
-            xc1,yc1 = self.get_node_center(node1)
-            min_dst2 = 10**10 # +Inf
-            best_node2  = -1
-            for node2 in self.node_list:
-                if node1 != node2:
-                    xc2,yc2 = self.get_node_center(node2)
-                    distance = (xc2-xc1)**2 + (yc2-yc1)**2
-                    if distance < min_dst2:
-                        min_dst2 = distance
-                        best_node2 = node2
-            if best_node2 != -1:
-                vertices.append([node1,best_node2])
-
-        return vertices
     
-    def get_mesh_connection_ALL(self):
-        vertices = []
-        for node1 in self.node_list:
-            for node2 in self.node_list:
-                vertices.append([node1,node2])
+    
+    
 
-        return vertices
+    def get_mesh_connection_matrix(self):
+        n = len(self.node_list)
+        dist_matrix = np.zeros((n,n))
+        for i,node1 in enumerate(self.node_list):
+            xc1,yc1 = self.get_node_center(node1)
+            for j,node2 in enumerate(self.node_list):                
+                xc2,yc2 = self.get_node_center(node2)
+                distance = (xc2-xc1)**2 + (yc2-yc1)**2
+                dist_matrix[i,j] = distance  
+        #return dist_matrix
 
-    def draw_all_lines(self):
+        m_base = np.ones((n+1,n+1))*1000000000
+        m_base[1:,1:] = dist_matrix
+        for i in range(len(self.node_list)):
+            if self.node_list[i].type == 'router':
+                m_base[0,i+1] = 0.1
+                m_base[i+1,0] = 0.1
+        m_base[0,0] = 0
+        print(np.round(m_base,3))
+        return m_base
+
+
+        
+        
+    def get_connections_list(self):
+        dist_matrix = self.get_mesh_connection_matrix()
+        self.dijkstra_graph.V = dist_matrix.shape[0]
+        self.dijkstra_graph.graph = dist_matrix
+
+        rx_connection_list = self.dijkstra_graph.dijkstra(0)
+        rx_connection_list = rx_connection_list[1:]
+        rx_connection_list = [c-1 for c in rx_connection_list]
+        return rx_connection_list
+
+
+    def draw_connections(self):
+        rx_connection_list = self.get_connections_list()
+
         for line in self.line_list.copy():
             self.canvas.delete(line)
             self.line_list.remove(line)
+        
+        for i in range(len(self.node_list)):            
+            #i = i-1
+            node_child  = self.node_list[i]
+            if node_child.type != 'router':
+                node_parent = self.node_list[rx_connection_list[i]]
+                xc1,yc1 = self.get_node_center(node_child)
+                xc2,yc2 = self.get_node_center(node_parent)
+                line = self.canvas.create_line(xc1,yc1,xc2,yc2)
+                self.line_list.append(line)
 
-        vertices = self.get_mesh_connection_ALL()
-        for pair in vertices:
-            node1, node2 = pair    
-            xc1,yc1 = self.get_node_center(node1)
-            xc2,yc2 = self.get_node_center(node2)
-            line = self.canvas.create_line(xc1,yc1,xc2,yc2)
-            self.line_list.append(line)
-    
+
             
+    def move_node(self, obj, event):
+        [x,y] = [event.x,event.y]
+        xc, yc = self.get_node_center(obj)
+        self.canvas.move(obj.ID, -(xc-x),-(yc-y))
+        self.canvas.move(obj.txt, -(xc-x),-(yc-y))
+        
+
     def drag_node_canvas(self, event):
+        '''
+        Callback for left mouse motion
+        '''
         [x,y] = [event.x,event.y]
         print(x,y)
         obj = self.check_click_intersection(x,y)
         if obj != -1:
-            xc, yc = self.get_node_center(obj)
-            self.canvas.move(obj.ID, -(xc-x),-(yc-y))
+            self.move_node(obj, event)
+
  
-        self.draw_all_lines()        
+        self.draw_connections()        
         print([c.ID for c in self.node_list])
 
 
-    
+    def delete_node(self, node):
+        self.canvas.delete(node.ID)
+        self.canvas.delete(node.txt)
+        self.node_list.remove(node)
+        
     def delete_node_canvas(self,event):
+        '''
+        Callback for right click
+        '''
         print("Delete node:")
         [x,y] = [event.x,event.y]
         obj = self.check_click_intersection(x,y)
         if obj != -1:       
-            self.canvas.delete(obj.ID)
-            self.node_list.remove(obj)
-        self.draw_all_lines()
+            self.delete_node(obj)
+        self.draw_connections()
 
     def delete_all_canvas(self, event):
+        '''
+        Callback for double right click
+        '''
         for node in self.node_list.copy():
-            self.canvas.delete(node.ID)
-            self.node_list.remove(node)
-        self.draw_all_lines()
-
-
-
+            self.delete_node(node)
+        self.draw_connections()
